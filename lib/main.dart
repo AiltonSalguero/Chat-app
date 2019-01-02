6c563a98a23419be9fc622270a15cc12a4463606
Kyssnar <ailton.salg@gmail.com>
2019-01-02 08:06:12 -0500

    Firebase and FLutter connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,117 @@
 import 'package:flutter/material.dart';
 
+//FireStore Plugin
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter and FireStore Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  ///Variable para agregar un brandname al collection
+  String _brandname;
+  int _sold;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        //---------------------
+        //Barra de navegacion
+        appBar: AppBar(
+          title: Text('Ailton Chat'),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              showInputDialog();
+            },
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        //---------------------
+
+        //---------------------
+        body: StreamBuilder(
+          stream: Firestore.instance.collection('car').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return Text('Loading data... Please wait.');
+            //Si tiene datos:
+            return Column(
+              children: <Widget>[
+                Text(snapshot.data.documents[0]['brandname']),
+                Text(snapshot.data.documents[0]['sold'].toString()),
+              ],
+            );
+          },
+        )
+        //---------------------
+
+        );
+  }
+
+  Future<Null> showInputDialog() async {
+      await showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text(
+              'Add a document',
+              style: TextStyle(fontSize: 14.0),
+            ),
+            children: <Widget>[
+              //Campos para ingresar los datos
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Brand name',
+                ),
+                onChanged: (value) {
+                  _brandname = value;
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Sold',
+                ),
+                onChanged: (value) {
+                  _sold = double.parse(value).round();
+                },
+              ),
+
+              //Boton para agregar
+              RaisedButton(
+                child: Text('Add'),
+                onPressed: saveDocument,
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+  saveDocument() {
+    //Guarda un nuevo documento in el firestore collection.
+    Firestore.instance
+        .collection('car')
+        .add({'brandname': _brandname, 'sold': _sold}).whenComplete(() {
+      print('Document added');
+    }).catchError((e) {
+      print(e);
+    });
   }
 }
